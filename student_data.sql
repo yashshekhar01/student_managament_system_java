b0db7d0264afecfee1492470e8e4aeb22548cace
Yash Shekhar <yashshekhar2934@gmail.com>
2024-11-05 18:06:54 +0530

    files added

---
+++ student_data.sql
@@ -0,0 +1,4 @@
+use studentmanagementsystem;
+show tables;
+create table student(studentId varchar(20) primary key,firstname varchar(20),lastname varchar(20),
+dateofbirth date,gender varchar(10),email varchar(30),phone varchar(20),marks int);
